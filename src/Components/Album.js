d54d0bfb77999687d08d84038c086db0a92aba87
Dennis Demir <dennisjdemir@gmail.com>
2023-06-19 05:24:06 -0400

    display album information

--- src/Components/Album.js
+++ src/Components/Album.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function Album({ music }){
     const {album, artist, image, runtime, genres} = music
-    console.log(album, artist, image, runtime, genres)
+
+    return (
+        <div>
+            <h1>{album}</h1>
+            <img src={image} />
+            <p>{artist}</p>
+            <p>{runtime}</p>
+            <div>{genres.map((genre) => <span key={genre}>{genre} </span>)}</div>
+        </div>
+    )
 }
 
 export default Album;
