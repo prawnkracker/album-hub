16b9c8af98e729e1b1edbceed118dd28ecb35ddf
Dennis Demir <dennisjdemir@gmail.com>
2023-06-19 13:46:21 -0400

    install react router and add routes for existing paths

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Home from './Home';
 import AlbumList from './AlbumList';
+import { Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <AlbumList />
+      <Switch>
+        <Route exact path='/'>
+            <Home />
+        </Route>
+        <Route path='/albumlist'>
+            <AlbumList />
+        </Route>
+      </Switch>
     </div>
   );
 }
