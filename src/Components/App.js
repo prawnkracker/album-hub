9d1ed032bb165d69eac3790174da3281cd0a4a90
Dennis Demir <dennisjdemir@gmail.com>
2023-06-20 12:24:45 -0400

    add navbar for ui navigation

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Home from './Home';
 import AlbumList from './AlbumList';
 import { Switch, Route } from "react-router-dom"
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route exact path='/'>
             <Home />
