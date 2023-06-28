b23ec0c523cf1a3d45096abf71a879f07c5f24bd
Dennis Demir <dennisjdemir@gmail.com>
2023-06-28 18:23:35 -0400

    add gif to homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,7 @@ function Home(){
             <h1>Welcome to Album Hub!</h1>
             <h3>Where you can find all your favorite albums!</h3>
             <p>Don't see your favorite album? Add it to the list and show it off to the community!</p>
+            <img src="https://media.tenor.com/0hjOGLFaQa0AAAAC/lofi-girl-lofi.gif" alt="Lofi Girl" />
         </div>
 )}
 
