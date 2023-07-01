6ee68ad48368e43c64776eb73f97b4c3f2421fa2
Dennis Demir <dennisjdemir@gmail.com>
2023-06-30 20:58:19 -0400

    remove <p> from home page and put it at bottom of albumlist page

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Album from "./Album"
 import GenreFilter from "./GenreFilter";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 function AlbumList({ albumsToDisplay, onFilterChange, filter, onFavoriteClick, favorites }){
     return (
@@ -16,6 +17,12 @@ function AlbumList({ albumsToDisplay, onFilterChange, filter, onFavoriteClick, f
                 />
                 })}
             </ul>
+            <p>Don't see your favorite album? Add it to the list and show it off to the community!</p>
+            <NavLink
+            to='/albumlist/newalbum'
+            exact>
+                Add an album
+            </NavLink>
         </div>
     )
 }
