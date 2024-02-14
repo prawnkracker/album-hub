004f9d23b5562a122b6c6b879a08b8f856387bfa
Dennis Demir <dennisjdemir@gmail.com>
2024-02-14 01:45:24 -0500

    update favorites to reflect empty or not

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -6,8 +6,8 @@ import AlbumSearch from "./AlbumSearch";
 function Favorites({ filter, favorites, onRemoveFromFavorites, onFilterChange, onSearch, search }){
     return (
         <div className="favorites-container">
-            <h1>Your Favorite Albums!</h1>
-            <GenreFilter filter={filter} onFilterChange={onFilterChange} /> <AlbumSearch onSearch={onSearch} search={search}/>
+            {favorites.length > 0 ? <div><h1>Your Favorite Albums!</h1><GenreFilter filter={filter} onFilterChange={onFilterChange} /> <AlbumSearch onSearch={onSearch} search={search}/></div> : "Add some favorites!"}
+            {/* <GenreFilter filter={filter} onFilterChange={onFilterChange} /> <AlbumSearch onSearch={onSearch} search={search}/> */}
             {favorites.map((album) => {
                 return (
                     <FavoriteCard
